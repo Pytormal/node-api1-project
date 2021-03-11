49c50d2504943fb33b6dd3f5653e948f8ab231b9
AlexAndrew Mallory <pytormal@live.com>
2021-03-10 19:27:16 -0800

    init start

--- api/server.js
+++ api/server.js
@@ -1,3 +1,12 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model.js')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// instance of the express app
+const server = express()
+
+// global middleware
+server.use(express.json)
+
+// export server
+module.exports = server; // EXPORT YOUR SERVER instead of {}
