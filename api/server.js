83005703bc986c9bec329e0357417c5b3f0205f0
AlexAndrew Mallory <pytormal@live.com>
2021-03-10 21:26:39 -0800

    completed

--- api/server.js
+++ api/server.js
@@ -6,22 +6,23 @@ const User = require("./users/model.js");
 const server = express();
 
 // global middleware
-server.use(express.json);
+server.use(express.json());
 
 ////////// end points //////////
 
-// [Get]
+// [Get] Completed
 server.get("/", (req, res) => {
   res.json({ hello: "to my self" });
 });
 
-// [Get]:id
+// [Get]:id Completed
 server.get("/api/users/:id", async (req, res) => {
   try {
-    const users = await User.findById(id);
-    if (users) {
-      res.json(users);
+    const user = await User.findById(id);
+    if (user) {
+      res.json(user);
     } else {
+        res.status(404).json({message: 'bad id'})
     }
   } catch (error) {
     console.log(error);
@@ -29,26 +30,26 @@ server.get("/api/users/:id", async (req, res) => {
   }
 });
 
-// [Get] all
+// [Get] all Completed
 server.get("/api/users", async (req, res) => {
   try {
-    const users = await User.find();
-    res.json(users);
+    const user = await User.find();
+    res.json(user);
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error });
   }
 });
 
-// [Post] create
+// [Post] create Completed
 server.post("/api/users", async (req, res) => {
-  const users = req.body;
+  const user = req.body;
 
-  if (!users.name || !users.bio) {
+  if (!user.name || !user.bio) {
     res.status(400).json({ message: "user name and bio required" });
   } else {
     try {
-      const newUser = await User.insert(users);
+      const newUser = await User.insert(user);
       res.status(200).json(newUser);
     } catch (error) {
       console.log(error);
@@ -57,14 +58,14 @@ server.post("/api/users", async (req, res) => {
   }
 });
 
-// [Put] update
+// [Put] update Completed
 server.put("/api/users/:id", async (req, res) => {
   const { id } = req.params;
-  const users = req.body;
+  const user = req.body;
   try {
-    const update = await User.update(id, users);
-    if (users) {
-      res.json(update);
+    const updatedUser = await User.update(id, user);
+    if (updatedUser) {
+      res.json(updatedUser);
     } else {
       res.status(400).json({ error: error });
     }
@@ -74,17 +75,45 @@ server.put("/api/users/:id", async (req, res) => {
   }
 });
 
-server.patch('/api/users/:id', async(req, res) = {
-    
-})
+// server.patch("/api/users/:id", async (req, res) => {
+//   const { id } = req.params;
+//   const user = req.body;
+//   try {
+//     const updatedUser = await User.update(id, user);
+//     if (updatedUser) {
+//       res.json(updatedUser);
+//     } else {
+//       res.status(400).json({ error: error });
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ error: error });
+//   }
+// });
 
-// [Delete] remove
-server.remove('/api/users/:id', async (req, res) => {
+// // // for model.js // // //
+// const modify = (id, changes) => {
+//   const user = users.find((user) => user.id === id);
+//   if (!user) return Promise.resolve(null);
+
+//   const updatedUser = { ...changes, id };
+//   users = users.map((d) => (d.id === id ? updatedUser : d));
+//   return Promise.resolve(updatedUser);
+// };
+
+// [Delete] remove Completed
+server.delete('/api/users/:id', async (req, res) => {
     const { id } = req.params;
     try {
-        const users = await User.remove(id);
+        const user = await User.remove(id);
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).json({message: 'bad id'})
+        }
     } catch (error) {
-        res.json(users)
+      console.log(error);
+      res.status(500).json({ error: error });
     }
 })
 
